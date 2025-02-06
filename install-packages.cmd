06dad2dce4cdf177f939188c15de45bb8ad67d35
dirkarnez <smalldirkalex@gmail.com>
2025-02-07 03:15:48 +0800

    - upload test on Windows

--- install-packages.cmd
+++ install-packages.cmd
@@ -1,5 +1,8 @@
 @echo off
-set PYTHON_DIR=%USERPROFILE%\Downloads\python-3.10.8-amd64-portable
+@REM set SOFTWARE_DIR=%USERPROFILE%\Downloads
+set SOFTWARE_DIR=D:\Softwares
+
+set PYTHON_DIR=%SOFTWARE_DIR%\python-3.10.8-amd64-portable
 set PATH=%PYTHON_DIR%;%PYTHON_DIR%\Scripts
 
 python -m pip install --upgrade pip
--- local-run.cmd
+++ local-run.cmd
@@ -1,12 +1,11 @@
 @echo off
-set PYTHON_DIR=%USERPROFILE%\Downloads\python-3.10.8-amd64-portable
+@REM set SOFTWARE_DIR=%USERPROFILE%\Downloads
+set SOFTWARE_DIR=D:\Softwares
 
-set PYTHON_VLC_MODULE_PATH=%USERPROFILE%\Downloads\vlc-3.0.21-win64
+set PYTHON_DIR=%SOFTWARE_DIR%\python-3.10.8-amd64-portable
+set PATH=%PYTHON_DIR%;%PYTHON_DIR%\Scripts
 
-set PATH=^
-%PYTHON_DIR%;^
-%PYTHON_DIR%\Scripts;
-
+set PYTHON_VLC_MODULE_PATH=%SOFTWARE_DIR%\vlc-3.0.21-win64\vlc-3.0.21
 python main.py
 
 pause
