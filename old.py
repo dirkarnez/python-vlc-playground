fab2758a5d2b3c36e7c7f3e62ba6ee3b0402617d
dirkarnez <smalldirkalex@gmail.com>
2025-02-07 02:55:29 +0800

    - change to tkinter

---
+++ old.py
@@ -0,0 +1,16 @@
+# import vlc
+ 
+# # creating vlc media player object
+# media = vlc.MediaPlayer("LS102 Linear Time-Invariant Systems.mp4")
+ 
+# # start playing video
+# media.play()
+
+
+# import vlc
+# instance = vlc.Instance('--fullscreen')
+# player = instance.media_player_new()
+# media = instance.media_new("LS102 Linear Time-Invariant Systems.mp4")
+# player.set_media(media)
+# player.play()
+
